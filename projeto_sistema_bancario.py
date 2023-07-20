e83fce4b902917325dd0e4e31831895e36d5fdfe
Nando <luiz2711fernando@gmail.com>
2023-07-20 17:53:19 -0300

    Finalizei a parte do depósito e extrato.

--- projeto_sistema_bancario.py
+++ projeto_sistema_bancario.py
@@ -5,7 +5,7 @@ menu = f"""
         MENU
 
     [1] Saque
-    [2] Depoósito
+    [2] Depósito
     [3] Visualizar extrato
     [4] Sair
 
@@ -15,8 +15,6 @@ saldo = 1000
 limite_saque = 3
 SAQUE_MAXIMO = 500.00
 extrato = ''
-
-
 
 repeticao = True
 while repeticao:
@@ -39,19 +37,28 @@ while repeticao:
                     else:
                         saldo -= saque
                         limite_saque -= 1
-                        extrato = extrato + f'-R${str(saque)} saldo={str(saldo)}  '
+                        extrato = extrato + f'-R${str(saque)} saldo=R${str(saldo)} | '
                 else:
                     print('Você precisa digitar um número!')
             else:
                 print('VOCÊ ATINGIU O LIMITE DE SAQUE')
                 break
         elif opcao == '2':
-            continue
+            deposito = input('Quanto você deseja depositar? ').strip()
+            if deposito.isnumeric() is False:
+                print('Você deve digitar um número!')
+            else:
+                saldo += float(deposito)
+                extrato = extrato + f'+R${deposito} saldo=R${saldo} | '
         elif opcao == '3':
-            print(extrato)
+            if extrato == '':
+                print('Seu extrato está vazio.')
+            else:
+                print(extrato)
         elif opcao == '4':
             break
 
+print('Obrigado por usar!')
 
         
 
